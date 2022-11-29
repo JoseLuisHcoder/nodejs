92a19acf1d6bca51af265d2aa1a1131b8b7b9b35
JoseLuisHcoder <zonajl@gmail.com>
2022-11-29 09:34:12 -0400

    upload project  blog nodejs

---
+++ proyecto1g/blogacdemlo/src/utils/crypto.js
@@ -0,0 +1,15 @@
+const bcrypt = require ('bcrypt')
+
+const hashPassword = (plainPassword) => {
+    // plainPassword = contraseña en texto plano
+    return bcrypt.hashSync(plainPassword, 10)
+}
+// console.log(hashPassword('Lucianita1*'));
+
+// para ver en la consola poner en la terminal  node ./src/utils/crypto.js
+
+const comparePassword = (plainPassword, hashedPassword) => {
+    return bcrypt.compareSync(plainPassword, hashedPassword)
+}
+//le pasamos los datos y mostramos en consola
+console.log(comparePassword('Lucianita1*', '$2b$10$t4pvzFAZH9JNvzfokAcq3uDUdiPXnsykddtQiLgodPXKtXBaBpkf2'));
