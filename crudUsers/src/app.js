5d5c082b914ffb56ed305e86fdbef2355ef27e6c
Jose Luis <zonajl@gmail.com>
2023-03-24 22:35:23 -0400

    ini project crudUsers

---
+++ crudUsers/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const port = 9000
+const app = express()
+
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'OK'})
+})
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+})
